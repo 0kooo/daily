ef26de12070e53694875193d077708d36c894668
360619623@qq.com <360619623@qq.com>
2022-11-18 10:16:41 +0800

    commit

--- day43/src/Text1/ListNode.java
+++ day43/src/Text1/ListNode.java
@@ -1,9 +1,9 @@
-package Text2;
+package Text1;
 
 public class ListNode {
     int val;
     ListNode next;
     ListNode() {}
-    public ListNode(int val) { this.val = val; }
+    ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
